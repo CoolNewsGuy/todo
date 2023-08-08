612cedfb69ab18ff8bc48e8609db81a5e24e776b
Aymen <aymendd3131@gmail.com>
2023-08-08 21:11:41 +0100

    Show an error for no task

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 import Todo from "./components/Todo.js";
 
 const newTaskTextInput = document.querySelector(".new-task-text");
+const noTaskErrorElement = document.querySelector(".no-task-error");
 const addBtn = document.querySelector("#task-adder-btn");
 const todosContainer = document.querySelector(".todos-container");
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+    if (newTaskTextInput.value === "") {
+        noTaskErrorElement.classList.remove("hidden");
+        noTaskErrorElement.classList.add("visible");
+        return;
+    }
+
+    noTaskErrorElement.classList.remove("visible");
+    noTaskErrorElement.classList.add("hidden");
     todosContainer.append(Todo(newTaskTextInput.value));
     newTaskTextInput.value = "";
-});
+}
