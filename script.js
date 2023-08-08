4e5ade757e85898960e2249710e9f0e618fe35b2
Aymen <aymendd3131@gmail.com>
2023-08-08 20:28:59 +0100

    Empty script.js file

--- script.js
+++
@@ -1,7 +0,0 @@
-let todoCheckbox = document.querySelectorAll(".todo-checkbox");
-
-todoCheckbox.forEach((checkbox) =>
-    checkbox.addEventListener("click", () => {
-        checkbox.classList.add("todo-checked");
-    })
-);
