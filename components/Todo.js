25b051be37dde411968021d99b3cd65c9d870156
Aymen <aymendd3131@gmail.com>
2023-08-08 20:44:56 +0100

    Refactor Todo and add a click event

--- components/Todo.js
+++ components/Todo.js
@@ -1,10 +1,21 @@
 export default function Todo(todoText) {
-    return `
-        <div class="todo">
-            <p class="todo-text">${todoText}</p>
-            <div class="todo-checkbox">
-                <div class="check-mark"></div>
-            </div>
-        </div>
-    `;
+    const todoDiv = document.createElement("div");
+    const todoCheckboxDiv = document.createElement("div");
+    const checkMarkDiv = document.createElement("div");
+    const todoTextPara = document.createElement("p");
+
+    todoDiv.className = "todo";
+    todoCheckboxDiv.className = "todo-checkbox";
+    checkMarkDiv.className = "check-mark";
+    todoTextPara.className = "todo-text";
+
+    todoCheckboxDiv.addEventListener("click", () => {
+        todoCheckboxDiv.classList.add("todo-checked");
+    });
+
+    todoTextPara.innerText = todoText;
+    todoCheckboxDiv.append(checkMarkDiv);
+    todoDiv.append(todoTextPara, todoCheckboxDiv);
+
+    return todoDiv;
 }
