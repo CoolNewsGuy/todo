54d78b72095ac1a47b68ca016c9a634ea8151a48
Aymen <aymendd3131@gmail.com>
2023-08-09 17:32:01 +0100

    Store todos in local storage

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,28 @@ export let removedTodosDivs = [];
 
 export function addTodoToTodosDivs(todoDiv) {
     todosDivs.push(todoDiv);
+    storeTodosInLocalStorage();
+    storeRemovedTodosInLocalStorage();
 }
 
 export function removeTodoFromTodosDivs(todoDiv) {
     removedTodosDivs.push(todosDivs.splice(todosDivs.indexOf(todoDiv), 1)[0]);
+    storeRemovedTodosInLocalStorage();
+    storeTodosInLocalStorage();
+}
+
+function storeTodosInLocalStorage() {
+    localStorage.setItem(
+        "todosDivs",
+        JSON.stringify(todosDivs.map((div) => div.querySelector("p").innerText))
+    );
+}
+
+function storeRemovedTodosInLocalStorage() {
+    localStorage.setItem(
+        "removedTodosDivs",
+        JSON.stringify(
+            removedTodosDivs.map((div) => div.querySelector("p").innerText)
+        )
+    );
 }
