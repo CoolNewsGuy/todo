dfec854d150c47126703fc02522a94a43dd1f911
Aymen <aymendd3131@gmail.com>
2023-08-09 06:57:09 +0100

    Add a function to refresh todos' container

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -1,1 +1,14 @@
-const UIUpdater = (() => {})();
+import Todo from "./components/Todo.js";
+import { todos } from "./script.js";
+
+const UIUpdater = (() => {
+    let todosContainer = document.querySelector(".todos-container");
+
+    function refreshTodosContainer() {
+        todosContainer.innerHTML = "";
+
+        todos.forEach((todo) => {
+            todosContainer.append(Todo(todo));
+        });
+    }
+})();
